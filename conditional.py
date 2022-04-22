b0a032727ef4cc217268618d16241cd3f0983012
HsinCL <hsinjohn1212@gmail.com>
2022-04-22 20:07:14 +0800

    add indoor.py

--- conditional.py
+++ conditional.py
@@ -1,5 +1,5 @@
 def is_even(n):
-    return True if n % 2 == 0 else False
+    return (n % 2 == 0)
 
 def main():
     x = int(input("What's x? "))
@@ -8,11 +8,9 @@ def main():
     else:
         print("Odd")
 
-    if 90 <= x <= 100:
-        print("A")
-    else:
-        print("below A")
-
-
+    #if 90 <= x <= 100:
+    #    print("A")
+    #else:
+    #    print("below A")
 
 main()
---
+++ indoor.py
@@ -0,0 +1,1 @@
+sentence = input("Input somthing: ").lower()
