87ed4305229c493f1dbee9ab0aa00272e1b991cb
HsinCL <hsinjohn1212@gmail.com>
2022-04-23 17:29:06 +0800

    solve deep thought

--- problem_set_1/deep.py
+++ problem_set_1/deep.py
@@ -1,4 +1,4 @@
-ans = input("What is the Answer to the Great Question of Life, the Universe, and Everthing?")
+ans = input("What is the Answer to the Great Question of Life, the Universe, and Everthing? ")
 valid_answer = ['42', 'forty-two', 'forty two']
 ans_str = str(ans).lower()
 if ans_str in valid_answer:
